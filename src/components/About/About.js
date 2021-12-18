eb33c00fab1e4b7ebf48601d23570a5f2e49b439
Chrus Chong <chrus1000@gmail.com>
2021-12-18 15:47:52 +0800

    Add media queries for small and medium view ports for responsiveness
    About Me section not responsive. Layout looks awkward on smaller
    viewports.
    
    Add media queries to ensure responsiveness of About Me section.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ const About = () => (
   <Section nopadding id='about'>
     <SectionDivider />
     <SectionTitle main>About Me</SectionTitle>
-    <div className='technologiesDiv'>
+    <div className='aboutDiv'>
       <Image
         src={aboutMeImage}
         layout='intrinsic'
@@ -21,20 +21,22 @@ const About = () => (
         width={400}
         height={400}
       />
-      <SectionText>
-        Wenna Ramsbottom was thinking about Marion Gloop again. Marion was a
-        brutal teacher with scrawny hands and slimy fingers. Wenna walked over
-        to the window and reflected on her deprived surroundings. She had always
-        loved deserted Bangkok with its bitter, big beaches. It was a place that
-        encouraged her tendency to feel worried. Then she saw something in the
-        distance, or rather someone. It was the a brutal figure of Marion Gloop.
-        Wenna gulped. She glanced at her own reflection. She was a mean,
-        special, brandy drinker with ginger hands and dirty fingers. Her friends
-        saw her as a vigilant, vague vicar. Once, she had even made a cup of tea
-        for a bewildered old lady. But not even a mean person who had once made
-        a cup of tea for a bewildered old lady, was prepared for what Marion had
-        in store today.
-      </SectionText>
+      <div className='aboutTextDiv'>
+        <SectionText>
+          Wenna Ramsbottom was thinking about Marion Gloop again. Marion was a
+          brutal teacher with scrawny hands and slimy fingers. Wenna walked over
+          to the window and reflected on her deprived surroundings. She had
+          always loved deserted Bangkok with its bitter, big beaches. It was a
+          place that encouraged her tendency to feel worried. Then she saw
+          something in the distance, or rather someone. It was the a brutal
+          figure of Marion Gloop. Wenna gulped. She glanced at her own
+          reflection. She was a mean, special, brandy drinker with ginger hands
+          and dirty fingers. Her friends saw her as a vigilant, vague vicar.
+          Once, she had even made a cup of tea for a bewildered old lady. But
+          not even a mean person who had once made a cup of tea for a bewildered
+          old lady, was prepared for what Marion had in store today.
+        </SectionText>
+      </div>
     </div>
   </Section>
 )
